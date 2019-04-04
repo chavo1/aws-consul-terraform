35f7541b33a529fd16a3b9e0ae42b0042b45607c
chavo1 <chavdar.rakov@gmail.com>
2019-04-04 10:42:29 +0300

    init

---
+++ variables.tf
@@ -0,0 +1,33 @@
+variable "access_key" {}
+variable "secret_key" {}
+variable "subnet" {}
+variable "instance_type" {}
+variable "key_name" {}
+variable "server_count" {}
+variable "dc_net" {}
+variable "dcname" {}
+
+// This is our server and client AMIs - we build with Packer for example and more info
+// https://github.com/chavo1/packer-consul-server
+
+variable "ami" {
+  type = "map"
+
+  default = {
+    server = "ami-00e9cbd23a90befc8"
+  }
+}
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "create" {
+  description = "Create Module, defaults to true."
+  default     = true
+}
+
+variable "name" {
+  description = "Name for resources, defaults to \"consul-auto-join-instance-role-aws\"."
+  default     = "consul-auto-join-aws"
+}
